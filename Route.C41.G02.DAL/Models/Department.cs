3ca8bb015155efad46221ca52115853b1c28cac9
Esraa156 <eo6344848@gmail.com>
2024-03-19 17:08:57 +0200

    DAL for Department Module

---
+++ Route.C41.G02.DAL/Models/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.C41.G02.DAL.Models
+{
+    internal class Department
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name is Required Ya Esraa!")]
+        public string Name { get; set; }
+        [Required]
+        public string Code { get; set; }
+        public DateTime DateOfCreation { get; set; }
+
+    }
+}
