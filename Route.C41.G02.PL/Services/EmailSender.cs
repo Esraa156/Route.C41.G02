4cf717611ea8d935584e1c68def312c0c1c748fd
Esraa156 <eo6344848@gmail.com>
2024-04-26 23:19:19 +0300

    Send Email

---
+++ Route.C41.G02.PL/Services/EmailSender.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using System.Net.Mail;
+using Microsoft.Extensions.Configuration;
+using System.Globalization;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+namespace Route.C41.G02.PL.Services
+{
+	public class EmailSender : IEmailSender
+	{
+
+		private readonly IConfiguration _configuration;
+
+		public EmailSender(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+		public async Task SendAsync(string from, string recipients, string subject, string body)
+		{
+			var SenderEmail = _configuration["EmailSettings:SenderEmail"];
+			var SenderPassword = _configuration["EmailSettings:SenderPassword"];
+			var emailMessage = new MailMessage();
+			emailMessage.From = new MailAddress(from);
+			emailMessage.To.Add(recipients);
+			emailMessage.Subject = subject;
+			emailMessage.Body = $"<html><body>{body}</body></html>";
+			emailMessage.IsBodyHtml = true;
+			var smtpClient = new SmtpClient(_configuration["EmailSettings:SmtpClientServer"], int.Parse(_configuration["EmailSettings:SmtpClientPort"]))
+			{
+				Credentials = new NetworkCredential(SenderEmail, SenderPassword),
+				EnableSsl = true
+			};
+			await smtpClient.SendMailAsync(emailMessage);
+		}
+
+		
+	}
+	}
+ 
+
---
+++ Route.C41.G02.PL/Services/IEmailSender.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Threading.Tasks;
+namespace Route.C41.G02.PL.Services
+{
+	public interface IEmailSender
+	{
+
+			Task SendAsync(StringInfo from, StringInfo recipients, string subject, string body);
+		}
+	}
+
